5c027daba27012b5bd1e8e7a0508995cb6396460
bstante <37376585+bstante@users.noreply.github.com>
2018-05-14 12:15:32 +0800

    First Commit
    Unity Project

---
+++ Uno Dos Tres/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+    
+
+    public LayerMask movementMask;
+    PlayerMotor motor;
+    Camera cam;
+	// Use this for initialization
+	void Start () {
+        motor = GetComponent<PlayerMotor>();
+        cam = Camera.main;
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, movementMask))
+            {
+                motor.MoveToPoint(hit.point);
+            }
+
+
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                
+            }
+
+
+        }
+    }
+    }
